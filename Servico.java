f9f053ae9454bb8cc8a40cdb43439bd2a50ea2c1
vicente944 <vicenteluiz07@gmail.com>
2026-09-09 18:21:17 -0300

    função exibirInfo implementada

--- Servico.java
+++ Servico.java
@@ -7,8 +7,19 @@ public class Servico {
         this.valor = valor;
     }
 
+    public static void exibirInfo() {
+        // Definição dos serviços disponíveis
+        Servico trocaOleo = new Servico("Troca de óleo do motor", 180.00);
+        Servico alinhamento = new Servico("Alinhamento de direção", 90.00);
+        Servico balanceamento = new Servico("Balanceamento de 4 rodas", 80.00);
+        Servico trocaPastilhas = new Servico("Troca de pastilhas de freio", 250.00);
+        Servico revisaoEletrica = new Servico("Revisão elétrica completa", 150.00);
 
-    public void exibirInfo() {
-
+        System.out.println("Servicos disponiveis: ");
+        System.out.println("1. " + trocaOleo.descricao + " .......... R$ " + trocaOleo.valor);
+        System.out.println("2. " + alinhamento.descricao + " .......... R$ " + alinhamento.valor);
+        System.out.println("3. " + balanceamento.descricao + " .......... R$ " + balanceamento.valor);
+        System.out.println("4. " + trocaPastilhas.descricao + " .......... R$ " + trocaPastilhas.valor);
+        System.out.println("5. " + revisaoEletrica.descricao + " .......... R$ " + revisaoEletrica.valor);
     }
 }
