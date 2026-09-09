2051516d528a5f8ec5afb8ba44a3887df23e086b
vicente944 <vicenteluiz07@gmail.com>
2026-09-09 18:20:49 -0300

    função adicionar ordem (não testada)

--- GerencOrdens.java
+++ GerencOrdens.java
@@ -1,8 +1,9 @@
 public class GerencOrdens {
     OS[] ordensServico = new OS[100];
 
-    public void addOrdem() {
-
+    // (Falta testar) Adiciona ordem de servico ao array do Gerenciador de Ordens
+    public void addOrdem(OS ordemEscolhida, int posicaoDaOrdem) {
+        ordensServico[posicaoDaOrdem] = ordemEscolhida;
     }
 
     public void listarOrdens() {
